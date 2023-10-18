f7b55d3ec460657910d72d6f5fa655154ec235d0
Samarth Bhardwaj <77209531+Samarthbhardwaj@users.noreply.github.com>
2023-10-18 12:19:54 +0530

    Update ThreeServoHexapod.ino

--- Hexapod/ThreeServoHexapod.ino
+++ Hexapod/ThreeServoHexapod.ino
@@ -9,13 +9,13 @@ SoftwareSerial BT(0,1);
 void setup() {
 Serial.begin(9600);
 BT.begin(9600);
-lift.attach(10);
+lift.attach(10);  //we are tellin arduino that which pins are in use
 rear.attach(11);
 front.attach(9);
 pinMode(12,OUTPUT);
 //now we are gonna take our hexa pod in start situation 
-lift.write(93);
-rear.write(90);
+lift.write(90);  
+rear.write(90);  // start position 
 front.write(90);
 delay(2000); // it will give the delay of 2 secs
 currstatus = 'S';
@@ -23,36 +23,32 @@ currstatus = 'S';
 void loop() {
 while (BT.available()){
 currstatus = BT.read();// this comaand is to read the new command if available
- if (currstatus=='W'){
-ledon();
+ if (currstatus=='W'){  
+ledon();                   //calling light on function
 }
 else if (currstatus=='w'){
-ledof();
+ledof();                    //calling light off function
 }
 }
 
 if (currstatus == 'F'){
-forward();
+forward();                  //calling forward function
 }
 else if (currstatus=='B'){
-back();
+back();                      //calling back function
 }
 else if (currstatus=='L'){
-
-left();
-
+left();                       //caling left function
 }
 else if (currstatus=='R'){
-  
-right();
-
+right();                       //calling right function
 }
 else if (currstatus=='S'){
-stop();
+stop();                        //calling stop function
 }
 }
-void forward(){
- lift.write (120);
+void forward(){                 //declearing forward function
+ lift.write (120);    
   delay(100);
   front.write(60);
   rear.write(60);
@@ -72,7 +68,7 @@ void forward(){
 
   
 }
-void back(){
+void back(){                    //declearing back function
   lift.write (120);
   delay(100);
   front.write(120);
@@ -92,32 +88,33 @@ void back(){
   delay(100);
 
 }
-void left(){
-lift.write(105);
+void left(){                   //declearing left function
+lift.write(110);
 delay(100);
 rear.write(60);
 delay(200);
 front.write(60);
 delay(200);
 rear.write(120);
+delay(400);
+front.write(120);
+delay(400);
+r
+}
+void right(){                  //declearing right function.
+lift.write(70);
+delay(100);
+rear.write(120);
 delay(200);
 front.write(120);
 delay(200);
-}
-void right(){
-lift.write(75);
-delay(100);
-rear.write(130);
-delay(250);
-front.write(130);
-delay(250);
-rear.write(55);
-delay(250);
-front.write(55);
-delay(250);
+rear.write(60);
+delay(400);
+front.write(60);
+delay(400);
 }
 
-void stop(){
+void stop(){                   //stop position function
   lift.write (90);
   delay(100);
   front.write(90);
@@ -125,9 +122,9 @@ void stop(){
   rear.write(90);
   delay(100);
 }
-void ledon(){
+void ledon(){        //light on function decleared
   digitalWrite(12,HIGH);
 }
-void ledof(){
+void ledof(){           // light of function
   digitalWrite(12,LOW);
 }
